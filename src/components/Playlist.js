c1b6b3f6348a1ed52701e2cbbecead96e7e5f0a7
Daniel Rodriguez <technoveltyco@users.noreply.github.com>
2023-03-16 21:42:35 +0000

    Add example or how to play song tracks

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -6,24 +6,35 @@ const tracks = tracksResults.hits.map((hit) => hit.track);
 
 console.log(artistsResults, tracksResults);
 function Playlist() {
+  const audio = new Audio(
+    "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/cb/24/ac/cb24ac75-128c-f817-7d81-7fbffbdaafe3/mzaf_1522770279362005862.plus.aac.ep.m4a"
+  );
+
   return (
-    <div>
-      <h2>Tracks</h2>
-      <ul className="Playlist">
-        {tracks.map((track) => {
-          console.log(track);
-          return (
-            <li key={track.key} className="Track">
-              <a
-                href={track.url}
-                target="_blank"
-                rel="noreferrer"
-              >{`${track.title} - ${track.subtitle}`}</a>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <aside className="w-auto pr-4">
+      <div className="bg-white shadow rounded-lg p-4 hover:shadow-lg cursor-pointer">
+        <h2 className="font-bold text-xl mb-2">Tracks</h2>
+        <ul className="Playlist font-bold text-xl mb-2">
+          <div class="grid grid-cols-1 gap-4">
+            {tracks.map((track) => {
+              return (
+                <li key={track.key} className="Track">
+                  <a
+                    onClick={(e) => {
+                      e.preventDefault();
+                      audio.play();
+                    }}
+                    href={track.url}
+                    target="_blank"
+                    rel="noreferrer"
+                  >{`${track.title} - ${track.subtitle}`}</a>
+                </li>
+              );
+            })}
+          </div>
+        </ul>
+      </div>
+    </aside>
   );
 }
 
