f49ad07d128d037bb3e620b565f3785a10c5ab18
Daniel Rodriguez <technoveltyco@users.noreply.github.com>
2023-03-19 01:19:23 +0000

    Add playlist loading in player

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,52 @@
-import shazamSong from "./mocks/shazam-song.json";
+import shazamSongs from "./mocks/shazam-search.json";
+// import shazamSong from "./mocks/shazam-song.json";
 
-const fetchSong = async (id) => {
-  const response = id ? shazamSong : null;
+const songMapping = (shazamSong) => {
+  const { key, title, subtitle, type, url, images, hub } = shazamSong;
+  return {
+    id: key,
+    title: title,
+    subtitle: subtitle,
+    type: type,
+    url: url,
+    images: {
+      background: images.background,
+      coverart: images.coverart,
+      coverarthq: images.coverarthq,
+    },
+    uriplayer: hub.actions.reduce((uri, action) => {
+      if (Object.keys(action).includes("uri")) {
+        uri = action.uri;
+      }
+      return uri;
+    }, ""),
+  };
+};
+
+const fetchSongs = async () => {
+  const { tracks } = shazamSongs;
+  const response = tracks.hits.map((hit) => {
+    const songMapped = songMapping(hit.track);
+    return songMapped;
+  });
   return await response;
 };
 
-export { fetchSong };
+const fetchSong = async (id) => {
+  if (id) {
+    const { tracks } = shazamSongs;
+    const response = tracks.hits
+      .filter((hit) => {
+        return hit.track.key === id;
+      })
+      .map((hit) => {
+        const songMapped = songMapping(hit.track);
+        return songMapped;
+      });
+
+    return await response[0];
+  }
+  return await {};
+};
+
+export { fetchSongs, fetchSong };
--- src/components/pages/DetailPage.js
+++ src/components/pages/DetailPage.js
@@ -1,23 +1,47 @@
+import { useEffect, useState } from "react";
+import { fetchSong } from "../../api/api.js";
 import Player from "../Player";
 import Playlist from "../Playlist";
 import "./DetailPage.css";
 
-function DetailPage({ song, tracks = false }) {
-  // console.log(song, tracks);
+const songIdMock = "40333609"; // key attribute from shazam json song
+
+function DetailPage({ songId = songIdMock, playlist = false }) {
+  const [song, setSong] = useState(false);
+  const [resetPlayer, setResetPlayer] = useState(false);
+
+  const getSong = async (songId) => {
+    const songFetched = await fetchSong(songId);
+    setSong(songFetched);
+  };
+
+  useEffect(() => {
+    getSong(songId);
+  }, [songId]);
+
+  const handleTrack = (songId, e) => {
+    e.preventDefault();
+    getSong(songId);
+    setResetPlayer(true);
+  };
+
   return (
     <main className="Page Details-page">
       <h1>Details Page</h1>
 
       <div className="Left-content">
-        <Player />
+        <Player song={song} reset={resetPlayer} />
       </div>
 
-      {tracks && (
+      {playlist && (
         <div className="Right-content">
           <aside className="w-auto pr-4">
             <div className="bg-white shadow rounded-lg p-4 hover:shadow-lg cursor-pointer">
               <h2 className="font-bold text-xl mb-2">Tracks</h2>
-              <Playlist />
+              <Playlist
+                tracks={playlist}
+                onClick={(id, e) => handleTrack(id, e)}
+              />
             </div>
           </aside>
         </div>
