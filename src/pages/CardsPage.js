6e414425d1fcd8adb15b16eb37fc98c15b9101ca
Daniel Rodriguez <technoveltyco@users.noreply.github.com>
2023-03-22 19:21:30 +0000

    Add link from Cards to music player

--- src/pages/CardsPage.js
+++ src/pages/CardsPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import SearchBar from "../components/SearchBar";
 import Aside from "../components/Aside";
 
@@ -89,7 +90,6 @@ function CardsPage({ tracks, setPlaylist }) {
   });
 
   const handleCardBtn = (key, e) => {
-    e.preventDefault();
     setPlaylist(hits.filter((hit) => hit.id === key));
   };
 
@@ -120,25 +120,27 @@ function CardsPage({ tracks, setPlaylist }) {
                       {item.title} - {item.artist}
                     </h4>
                     <p className="mb-2 leading-normal">{item.genre}</p>
-                    <button
-                      className="px-6 py-2 text-sm text-white bg-red-400 rounded shadow"
-                      onClick={(e) => handleCardBtn(item.id, e)}
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={3}
-                        stroke="currentColor"
-                        className="w-6 h-6"
+                    <Link to="/musicplayer">
+                      <button
+                        className="px-6 py-2 text-sm text-white bg-red-400 rounded shadow"
+                        onClick={(e) => handleCardBtn(item.id, e)}
                       >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
-                        />
-                      </svg>
-                    </button>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={3}
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+                          />
+                        </svg>
+                      </button>
+                    </Link>
                   </div>
                 </div>
               ))}
