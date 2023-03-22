e4d1704117834b9f4ce2fe2df72377fc04142550
unknown <eliasjozef01@gmail.com>
2023-03-22 01:22:25 +0000

    header folder for considering

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Header from "./components/Header";
+import Header from "./components/header/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import "./App.css";
